9e92fd664e7680659e32099ae265fbf41700cb0c
Surjit Sidhu <mail@surjitsidhu.com>
2022-10-21 09:15:28 -0600

    filters

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Dropdown from "./dropdown/dropdown";
 import DropContent from "./DropContent";
 import './App.css';
 
 function App() {
+    const [filters, setFilters] = useState<any>([])
+
+    const handleClick = () => {
+        setFilters((prevState: any) => {
+            return [...prevState, {show: true, name: 'filter 1'}]
+        })
+    }
+
     return (
         <div className="flex mt-5 ml-5">
-            <Dropdown showx={true}>
-                <Dropdown.Toggle>text</Dropdown.Toggle>
-                <Dropdown.Content>
-                    <DropContent/>
-                </Dropdown.Content>
-            </Dropdown>
+            <button onClick={handleClick}>Add filter</button>
+
+            {filters.map((filter: any, index: any) => {
+                return (
+                    <div key={index} className="flex mt-5 ml-5">
+                        <Dropdown showx={filter.show}>
+                            <Dropdown.Toggle>{filter.name}</Dropdown.Toggle>
+                            <Dropdown.Content>
+                                <DropContent/>
+                            </Dropdown.Content>
+                        </Dropdown>
+                    </div>
+                )
+            })}
         </div>
     );
 }
